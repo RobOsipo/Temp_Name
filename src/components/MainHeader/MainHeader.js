7c3d5add605ff521870b8c966fd82f2c2a122b4b
Robert <osipovitchr@gmail.com>
2022-04-18 16:09:18 -0400

    completed useReducer logic + deleted useStates

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>?Not sure what this app is yet?</h1>
+      <h1>?Not sure what this app is yet?</h1> 
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
